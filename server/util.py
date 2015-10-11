2d186a82f1fddf690fc9c610c22d279b5a6532ac
Alvin <alvin.lin@stuypulse.com>
2015-10-11 23:43:47 +0000

    added checkUsername() method to util

--- server/util.py
+++ server/util.py
@@ -2,9 +2,22 @@
 # Author: Alvin Lin (alvin.lin@stuypulse.com)
 
 import hashlib
+import re
 
 class Util():
 
   @staticmethod
   def hash(text):
     return hashlib.sha256(text).hexdigest()
+
+  @staticmethod
+  def checkUsername(username):
+    return not re.search('[^a-zA-Z0-9]', username) and len(username) > 0
+
+if __name__ == '__main__':
+  print Util.hash('what the')
+  print Util.checkUsername('omgimanerd')
+  print Util.checkUsername('omgimanerd1')
+  print Util.checkUsername('')
+  print Util.checkUsername('uh')
+  print Util.checkUsername('A#F#')
