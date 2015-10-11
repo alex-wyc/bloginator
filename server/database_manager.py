6ba28c0cd2b9e1fc461002ff3a6bbef00754588b
Alvin <alvin.lin@stuypulse.com>
2015-10-11 23:36:10 +0000

    updated database_manager

--- server/database_manager.py
+++ server/database_manager.py
@@ -9,8 +9,8 @@ from util import Util
 DATABASE = 'db/bloginator.db'
 
 class DatabaseManager():
-  def __init__(self, connection):
-    self.connection = connection
+  def __init__(self, database):
+    self.database = database
 
   @staticmethod
   def create():
@@ -35,21 +35,51 @@ class DatabaseManager():
               """)
     connection.commit()
     connection.close()
-    return DatabaseManager(connection)
+    return DatabaseManager(DATABASE)
 
+  # This registers a user and adds them to the database assuming all validity
+  # checks have passed on the username except for uniqueness. This function
+  # will return True if the registration was successful and False if there
+  # already exists a user with given username.
   def register_user(self, username, password):
-    self.connection = sqlite3.connect(DATABASE);
-    c = self.connection.cursor()
+    connection = sqlite3.connect(self.database);
+    c = connection.cursor()
     result = True
     try:
       c.execute('INSERT INTO users VALUES (?, ?)',
                 (username, Util.hash(password)))
     except sqlite3.IntegrityError:
       result = False
-    self.connection.commit()
-    self.connection.close()
+    connection.commit()
+    connection.close()
     return result
+
+  def fetch_all_users(self):
+    connection = sqlite3.connect(self.database)
+    c = connection.cursor()
+    c.execute('SELECT * FROM users');
+    users = c.fetchall()
+    connection.close()
+    return users
+
+  def fetch_all_posts(self):
+    connection = sqlite3.connect(self.database)
+    c = connection.cursor()
+    c.execute('SELECT * FROM posts')
+    posts = c.fetchall()
+    connection.close()
+    return posts
+
+  def fetch_all_comments(self):
+    connection = sqlite3.connect(self.database)
+    c = connection.cursor()
+    c.execute('SELECT * FROM comments')
+    comments = c.fetchall()
+    connection.close()
+    return comments
 
 if __name__ == '__main__':
   d = DatabaseManager.create()
   print d.register_user('username', 'password')
+  print d.register_user('bob', 'de bilder')
+  print map(lambda x: x[0], d.fetch_all_users())
