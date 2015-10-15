5b533dd698a8d22f6f433b37900e7638f27adc48
Alvin Lin <alvin.lin@stuypulse.com>
2015-10-15 12:30:27 -0400

    documentation to database manager

--- server/database_manager.py
+++ server/database_manager.py
@@ -36,11 +36,12 @@ class DatabaseManager():
     connection.commit()
     connection.close()
     return DatabaseManager(DATABASE)
-
-  # This registers a user and adds them to the database assuming all validity
-  # checks have passed on the username except for uniqueness. This function
-  # will return True if the registration was successful and False if there
-  # already exists a user with given username.
+  """
+  This registers a user and adds them to the database assuming all validity
+  checks have passed on the username except for uniqueness. This function
+  will return True if the registration was successful and False if there
+  already exists a user with given username.
+  """
   def register_user(self, username, password, fullname):
     connection = sqlite3.connect(self.database);
     c = connection.cursor()
@@ -53,17 +54,29 @@ class DatabaseManager():
     connection.commit()
     connection.close()
     return result
-
+  
+  """
+  This checks if a user is authorized given their username and password.
+  Returns True if and only if the given user exists and the given password
+  matches the stored password. Returns False if the given user does not
+  exist or the given password does not match the stored one.
+  """
   def is_user_authorized(self, username, password):
     connection = sqlite3.connect(self.database)
     c = connection.cursor()
     # We can assume username is a unique field.
     c.execute('SELECT password FROM users WHERE username=?',
               (username,))
-    actual_password = str(c.fetchone()[0])
+    actual_password = c.fetchone()
     connection.close()
-    return actual_password == Util.hash(password)
+    if actual_password:
+      return actual_password[0] == Util.hash(password)
+    return False
 
+  """
+  This method adds a post into the database given the username of the person
+  posting and the content of the post.
+  """
   def add_post(self, username, content):
     connection = sqlite3.connect(self.database)
     c = connection.cursor()
@@ -72,6 +85,9 @@ class DatabaseManager():
     connection.commit()
     connection.close()
 
+  """
+  This method fetches all the data we have stored on registered users.
+  """
   def fetch_all_users(self):
     connection = sqlite3.connect(self.database)
     c = connection.cursor()
@@ -80,6 +96,9 @@ class DatabaseManager():
     connection.close()
     return users
 
+  """
+  This method fetches all the data we have stored on user posts.
+  """
   def fetch_all_posts(self):
     connection = sqlite3.connect(self.database)
     c = connection.cursor()
@@ -88,6 +107,9 @@ class DatabaseManager():
     connection.close()
     return posts
 
+  """
+  This method fetches all the data we have stored on user comments.
+  """
   def fetch_all_comments(self):
     connection = sqlite3.connect(self.database)
     c = connection.cursor()
